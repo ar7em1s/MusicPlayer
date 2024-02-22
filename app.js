5c63f88489c0bb75b920478700e9ef9d49c21d47
ar7em1s <126914551+ar7em1s@users.noreply.github.com>
2024-02-22 17:41:03 +0200

    fix auto-start

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let progress = document.getElementById("progress");
 let song = document.getElementById("song");
 let ctrlIcon = document.getElementById("play-icon");
+let isPlaying = false;
 
 song.onloadedmetadata = function() {
     progress.max = song.duration;
@@ -10,25 +11,35 @@ song.onloadedmetadata = function() {
 function playPause() {
     if (ctrlIcon.classList.contains("fa-pause")) {
         song.pause();
+        let isPlaying = false;
         ctrlIcon.classList.remove("fa-pause");
         ctrlIcon.classList.add("fa-play");
     } else {
         song.play();
+        isPlaying = true;
         ctrlIcon.classList.add("fa-pause");
         ctrlIcon.classList.remove("fa-play");
     }
 }
-
-if(song.play()) {
-    setInterval(() => {
-        progress.value = song.currentTime;
-    }, 500);
+song.onplay = function() {
+    isPlaying = true;
 }
+
+song.onpause = function() {
+    isPlaying = false;
+}
+
+setInterval(() => { //prevents the auto-start on load; fix #1
+    if (isPlaying) {
+        progress.value = song.currentTime;
+    }
+}, 500);
+
 
 progress.onchange = function() {
     song.play();
+    isPlaying = true;
     song.currentTime = progress.value;
     ctrlIcon.classList.add("fa-pause");
     ctrlIcon.classList.remove("fa-play");
 }
-
